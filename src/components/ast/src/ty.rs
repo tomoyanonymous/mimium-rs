700e4b2c40daa4a30a816390b9b04a33881cb95b
Tomoya Matsuura(MacBookPro) <me@matsuuratomoya.com>
2022-06-28 11:04:38 +0900

    wrote experimental type systems

--- src/components/ast/src/ty.rs
+++ src/components/ast/src/ty.rs
@@ -1,16 +1,45 @@
-use crate::{
-    metadata::{WithMeta},
-};
+use crate::metadata::WithMeta;
+use std::fmt;
 
 #[derive(Clone, Debug, PartialEq)]
-pub enum Type{
-    Int(i64),
-    Float(f64),
-    String(String),
+pub enum Type {
+    //basic types
+    Unit,
+    Int,
+    Numeric,
+    String,
+    //aggregate types
+    Array(Box<WithMeta<Self>>),
     Tuple(Vec<WithMeta<Self>>),
-    Struct(Vec<(String,WithMeta<Self>)>),
-    Function{
-        parameters: Vec<WithMeta<Self>>,
-        body: Box<WithMeta<Self>>,
+    Struct(Vec<(String, WithMeta<Self>)>),
+    //Function that has a vector of parameters, return type, and type for internal states.
+    Function(
+        Vec<WithMeta<Self>>,
+        Box<WithMeta<Self>>,
+        Option<Box<WithMeta<Self>>>,
+    ),
+    //(experimental) code-type for multi-stage computation that will be evaluated on the next stage
+    Code(Box<WithMeta<Self>>),
+    //intermediate type for type inference
+    Intermediate(i64),
+}
+
+impl fmt::Display for Type {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Type::Unit => write!(f, "()"),
+            Type::Int => write!(f, "int"),
+            Type::Numeric => write!(f, "num"),
+            Type::String => write!(f, "string"),
+            Type::Array(a) => write!(f, "[{}]", a.0),
+            Type::Tuple(v) => write!(f, "({:?})", v),
+            Type::Struct(s) => write!(f, "{{{:?}}}", s),
+            Type::Function(p, r, s) => {
+                write!(f, "({:?})->", p)?;
+                write!(f, "{:?}{:?}", r, s)
+            }
+            Type::Code(c) => write!(f, "<{:?}>", c),
+            Type::Intermediate(id) => write!(f, "intermediate({})", id),
+        }
     }
 }
