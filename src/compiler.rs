08e487bb6912f4fadf0f3d4b7b61e5164edf8288
Tomoya(MacbookAir) <me@matsuuratomoya.com>
2023-09-06 20:39:41 +0900

    add emit-ast stage

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,8 +1,21 @@
 pub mod parser;
-pub mod selfconvert;
 pub mod recursecheck;
+pub mod selfconvert;
+
+use crate::{
+    ast,
+    utils::{error::ReportableError, metadata::WithMeta},
+};
+pub fn emit_ast(src: &String) -> Result<WithMeta<ast::Expr>, Vec<Box<dyn ReportableError>>> {
+    let rawast = parser::parse(src)?;
+    let res1 = recursecheck::convert_recurse(&rawast);
+    selfconvert::convert_self_top(res1).map_err(|e| {
+        let bres = Box::new(e) as Box<dyn ReportableError>;
+        vec![bres]
+    })
+}
+
 pub mod typing;
 
 // pub mod hirgen;
 pub mod mirgen;
-
