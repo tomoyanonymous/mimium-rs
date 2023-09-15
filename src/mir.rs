0638d21e8caff9b5dbfc33f33f3c8d6a9a23baf8
松浦 知也 Matsuura Tomoya <me@matsuuratomoya.com>
2023-09-15 20:54:19 +0900

    wip mir

--- src/mir.rs
+++ src/mir.rs
@@ -2,50 +2,87 @@
 use std::rc::Rc;
 pub struct Label(String);
 
-pub enum Value {
-    Register(Rc<Label>),
-    Address(u64),
+// is it OK as same as mimium base typing?
+pub enum Type{
+    Unit
+}
+
+
+pub enum Value{
+    // holds SSA index(position in infinite registers)
+    Register(u64,Type,Option<Label>),
+    // immidiate mode floating point value
+    Float(f64),
     Integer(i64),
-    Numeric(String),
-    FuncRef(Rc<Label>),
-    FParam(Rc<Label>),
-    External(Rc<Label>),
+    Bool(bool),
+    // pointer to the top-level functions
+    Function(Label),
+    //
+    Closure(Label,Vec<Box<Value>>),
+    //??
 }
-pub enum Intrinsic {
-    Add,
-    Sub,
-    Mult,
-    Div,
-    Mod,
-    Ge,
-    Gt,
-    Le,
-    Lt,
-}
+
 pub enum Instruction {
-    Const(Value),
+    // allocate appropreate memory size depending on the type and return its pointer address
+    Alloc(Type),
+    // load value from the pointer type
     Load(Value),
-    Store(Value, Value), //from,to
-    Tuple(Vec<Value>),
-    Proj(Value, u64),
-    Call(Label, Vec<Value>),
+    // store value to pointer
+    Store(Value, Value),
+    // Tuple(Vec<Value>),
+    // Proj(Value, u64),
+    // call function 
+    Call(Value, Vec<Value>),
     CallIntrinsic(Intrinsic, Vec<Value>),
-    MakeClosure(Label, Vec<Value>),
-    If(Value,Block,Block),
-    Return(Value)
+    Closure(Label, Vec<Value>),
+    //function offset  and localvar offset?
+    UpValue(u64,u64),
+    JmpIf(Value,Label,Label),
+    Return(Value),
+
+    // Primitive Operations
+    AddF(Value,Value),
+    SubF(Value,Value),
+    MulF(Value,Value),
+    DivF(Value,Value),
+    ModF(Value,Value),
+    NegF(Value),
+    SinF(Value),
+    CosF(Value),
+    PowF(Value,Value),
+    LogF(Value,Value),
+
+    // Primitive Operations for int
+    AddI(Value,Value), 
+    SubI(Value,Value),
+    MulI(Value,Value),
+    DivI(Value,Value),
+    ModI(Value,Value),
+    NegI(Value),
+    PowI(Value),
+    LogI(Value,Value),
+    // primitive Operations for bool
+    Not(Value),
+    Eq(Value),
+    Ne(Value),
+    Gt(Value,Value),
+    Ge(Value,Value),
+    Lt(Value,Value),
+    Le(Value,Value),
+    And(Value,Value),
+    Or(Value,Value),
+
+
 }
 
 pub struct Block(Vec<Instruction>);
 
 pub struct Function {
-    pub params: Vec<Rc<Label>>,
-    pub clsref: Option<Rc<Label>>,
-    pub selfref: Option<Rc<Label>>,
-    pub body: Block,
+    pub body: Vec<Block>,
 }
 
 pub enum TopLevel{
     Function(Function),
-    //other declaration continues...
+    //other global declaration continues...
 }
 pub struct Mir(pub Vec<TopLevel>);
