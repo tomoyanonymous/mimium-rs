d70b71cd04d3d38ed518ba624fe6090df0140497
Hiroaki Yutani <yutani.ini@gmail.com>
2024-09-06 16:14:05 +0900

    Validate schedule before setting

--- mimium-lang/src/runtime/vm.rs
+++ mimium-lang/src/runtime/vm.rs
@@ -836,6 +836,8 @@ impl Machine {
         self.execute(0, prog, None)
     }
     pub fn execute_task(&mut self, now: Time, prog: &Program) {
+        self.scheduler.set_cur_time(now);
+
         if let Some(task_cls) = self.scheduler.pop_task(now, prog) {
             log::debug!("task id {:?}", task_cls);
             let closure = self.get_closure(task_cls);
