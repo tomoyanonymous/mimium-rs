5a0ef1039cf039b7a7b60b54a371909d3e2415c0
松浦 知也 Matsuura Tomoya <me@matsuuratomoya.com>
2024-09-09 18:30:40 +0900

    fixed incorrect comparison

--- mimium-lang/src/interner.rs
+++ mimium-lang/src/interner.rs
@@ -136,13 +136,13 @@ pub struct TypeNodeId(pub TypeKey);
 
 impl PartialEq for ExprNodeId {
     fn eq(&self, other: &Self) -> bool {
-        self.to_expr() == other.to_expr() && self.to_span() == self.to_span()
+        self.to_expr() == other.to_expr() && self.to_span() == other.to_span()
     }
 }
 
 impl PartialEq for TypeNodeId {
     fn eq(&self, other: &Self) -> bool {
-        self.to_type() == other.to_type()
+        self.to_type() == other.to_type() && self.to_span() == other.to_span()
     }
 }
 impl Eq for TypeNodeId {}
