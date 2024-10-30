3018edc9cd8528d66bc3131c5a4a9afbdc9e63ea
松浦 知也 Matsuura Tomoya <me@matsuuratomoya.com>
2024-10-30 22:24:48 +0900

    midiin wip2

--- mimium-midi/src/lib.rs
+++ mimium-midi/src/lib.rs
@@ -1,14 +1,28 @@
-use std::cell::OnceCell;
-
+use atomic_float::AtomicF64;
 use midir::{MidiInput, MidiInputPort};
 use mimium_lang::{
+    function,
+    interner::ToSymbol,
+    numeric,
     plugin::{SysPluginSignature, SystemPlugin},
     runtime::{vm, Time},
 };
+use std::{
+    cell::{OnceCell, RefCell},
+    rc::Rc,
+    sync::Arc,
+};
+
+type NoteCallBack = Box<dyn Fn(f64, f64) -> ()>;
+
+#[derive(Default)]
+struct NoteCallBacks(pub [Vec<NoteCallBack>; 16]);
+
 pub struct MidiPlugin {
     input: MidiInput,
     port: OnceCell<MidiInputPort>,
     port_name: Option<String>,
+    note_callbacks: NoteCallBacks,
 }
 
 impl MidiPlugin {
@@ -18,7 +32,39 @@ impl MidiPlugin {
             input,
             port: OnceCell::new(),
             port_name: None,
+            note_callbacks: Default::default(),
         })
+    }
+    fn add_note_callback(&mut self, chan: u8, cb: NoteCallBack) {
+        if chan < 15 {
+            self.note_callbacks.0[chan as usize].push(cb);
+        }
+    }
+    fn invoke_note_callback(&self, chan: u8, note: u8, vel: u8) {
+        if chan < 15 {
+            self.note_callbacks.0[chan as usize]
+                .iter()
+                .for_each(|cb| cb(note as f64, vel as f64));
+        }
+    }
+    pub fn bind_midi_note_mono(&mut self, vm: &mut vm::Machine) -> vm::ReturnCode {
+        let ch = vm::Machine::get_as::<f64>(vm.get_stack(0));
+        let cell = Arc::new((AtomicF64::new(), AtomicF64::new()));
+        self.add_note_callback(ch, |note, vel| {
+            let (note_c, vel_c) = cell.clone();
+            note_c.write(note);
+            vel_c.write(vel);
+        });
+        let cls = |vm: &mut vm::Machine| -> vm::ReturnCode {
+            let (note_c, vel_c) = cell.clone();
+            vm.set_stack(0, vm::Machine::to_value(note_c));
+            vm.set_stack(1, vm::Machine::to_value(vel_c));
+            2
+        };
+        let ty = function!(vec![], numeric!());
+        let rcls = vm.wrap_extern_cls(("get_midi_val".to_symbol(), Rc::new(RefCell::new(cls)), ty));
+        vm.set_stack(0, vm::Machine::to_value(rcls));
+        1
     }
 }
 
@@ -56,6 +102,10 @@ impl SystemPlugin for MidiPlugin {
     }
 
     fn gen_interfaces(&self) -> Vec<SysPluginSignature> {
-        todo!()
+        let ty = function!(vec![numeric!()], function!(vec![], numeric!()));
+
+        let bindnote =
+            SysPluginSignature::new("bind_midi_note_mono", Self::bind_midi_note_mono, ty);
+        vec![bindnote]
     }
 }
