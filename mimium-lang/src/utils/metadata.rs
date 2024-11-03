0b8c69e268da78b01969d3b6a8cb9776a0da8b1e
Hiroaki Yutani <yutani.ini@gmail.com>
2024-11-04 08:58:44 +0900

    Fix clippy::redundant_static_lifetimes

--- mimium-lang/src/utils/metadata.rs
+++ mimium-lang/src/utils/metadata.rs
@@ -5,7 +5,7 @@ pub type Span = std::ops::Range<usize>;
 //     pub location: Span,
 //     pub value : T
 // }
-pub(crate) const GLOBAL_LABEL: &'static str = "_mimium_global";
+pub(crate) const GLOBAL_LABEL: &str = "_mimium_global";
 
 // #[derive(Clone, Debug, PartialEq)]
 // pub struct WithMeta<T: Clone + PartialEq>(pub T, pub Span);
