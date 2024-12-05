1536ffe67c32cec673b932a5693f236716b838d6
松浦 知也 Matsuura Tomoya <me@matsuuratomoya.com>
2024-12-05 18:18:53 +0900

    fix error type in macro expand

--- mimium-lang/src/compiler/parser/resolve_include.rs
+++ mimium-lang/src/compiler/parser/resolve_include.rs
@@ -1,12 +1,12 @@
-use super::{parse, Span};
-use crate::interner::ExprNodeId;
-use crate::utils::error::{ReportableError, ReportableErrorDyn};
+use super::{parse, Location, Span};
+use crate::interner::{ExprNodeId, ToSymbol};
+use crate::utils::error::{ReportableError, SimpleError};
 use crate::utils::fileloader;
 
-fn make_vec_error<E: std::error::Error>(e: E, span: Span) -> Vec<Box<dyn ReportableError>> {
-    vec![Box::new(ReportableErrorDyn {
+fn make_vec_error<E: std::error::Error>(e: E, loc: Location) -> Vec<Box<dyn ReportableError>> {
+    vec![Box::new(SimpleError {
         message: e.to_string(),
-        span: span.clone(),
+        span: loc,
     }) as Box<dyn ReportableError>]
 }
 
@@ -15,9 +15,13 @@ pub(super) fn resolve_include(
     target_path: &str,
     span: Span,
 ) -> Result<ExprNodeId, Vec<Box<dyn ReportableError>>> {
+    let loc = Location {
+        span: span.clone(),
+        path: mmm_filepath.to_symbol(),
+    };
     let abspath = fileloader::get_canonical_path(mmm_filepath, target_path)
-        .map_err(|e| make_vec_error(e, span.clone()))?;
+        .map_err(|e| make_vec_error(e, loc.clone()))?;
     let content =
-        fileloader::load(abspath.to_str().unwrap()).map_err(|e| make_vec_error(e, span.clone()))?;
+        fileloader::load(abspath.to_str().unwrap()).map_err(|e| make_vec_error(e, loc))?;
     parse(&content, Some(abspath))
 }
